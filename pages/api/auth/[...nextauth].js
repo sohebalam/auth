a08d96eab23bfee7edd888d678684a540be206b7
soheb alam <sohebalam@hotmail.com>
2022-01-17 21:01:32 +0000

    second

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -33,5 +33,12 @@ export default NextAuth({
 
       return session
     },
+    session: {
+      strategy: "database",
+
+      maxAge: 30 * 24 * 60 * 60, // 30 days
+
+      updateAge: 24 * 60 * 60, // 24 hours
+    },
   },
 })
